011aa7fcb9b7f22f15d942f14bc84baf3b6fb564
Marcelo de Matos Gomez <marcelo.matos.gomez@gmail.com>
2025-03-05 00:07:49 -0300

    Initial project structure.

---
+++ src/MoneyMaster.IoC/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MoneyMaster.Application;
+using MoneyMaster.Infrastructure;
+
+namespace MoneyMaster.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAllServices(this IServiceCollection services)
+            => services
+                .AddApplicationServices()
+                .AddInfrastructureServices();
+    }
+}
